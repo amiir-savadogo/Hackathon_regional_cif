eb80b2bdcf8388959e09dc50cfc02d5ab3417d41
Abdrafith ZONGO <zongo1abdrafith@gmail.com>
2026-08-21 08:42:43 +0000

    Configuration de l'URL finale du backend pour la production

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-java-render.onrender.com/api' // Remplacer par l'URL finale Render
+  apiUrl: 'https://creditsur-backend.onrender.com/api'
 };
